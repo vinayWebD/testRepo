258f76b529c3b8e3cce6ce9f87657fe8992ae509
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-08-25 17:41:41 +0530

    code setup - env files and modified prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,19 +8,27 @@ module.exports = {
   globals: {
     test: true,
   },
-  extends: ['airbnb', 'plugin:react/recommended', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect', // or specify the version you are using, e.g., '16.14.0'
+    },
+  },
+  extends: ['plugin:react/recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // Allow JSX to be present in files with either a .js or .jsx extension
+    'react/react-in-jsx-scope': 'off', // Allow jsx to be run even if there is no React imported
+    eqeqeq: 'error', // Enforce using === and !== over == and !=
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }], // Disallow unused variables,
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }], // Enforce spacing around colon in object literals,
+    quotes: ['error', 'single'], // or "double"
   },
   parserOptions: {
     allowImportExportEverywhere: true,
     ecmaVersion: 2023, // Specify the ECMAScript version you're using
-    sourceType: 'module', // Indicate that you're using ES modules
+    sourceType: 'module', // Indicates that the code is using ES modules (import/export syntax)
     ecmaFeatures: {
       jsx: true, // Enable JSX parsing
     },
-    quotes: ['error', 'single'], // or "double"
   },
 };
