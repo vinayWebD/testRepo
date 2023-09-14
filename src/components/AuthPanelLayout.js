4ab504d49dae43f6384831db5b1ad6dc3c38f352
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-09-14 12:02:37 +0530

    fixed responsiveness issues

--- src/components/AuthPanelLayout.js
+++ src/components/AuthPanelLayout.js
@@ -7,15 +7,15 @@ const { LANG_AUTH_PANEL_LAYOUT_HEADING } = LANG.PAGES.AUTH_COMMON;
 
 function AuthPanelLayout({ children }) {
   return (
-    <div className="flex justify-start items-start h-[100vh] sm:flex-col sm:flex-wrap relative">
-      <div className=" bg-darkblue h-[100vh] relative hidden lg:block lg:w-[50%] w-[100%]">
+    <div className="flex justify-start min-h-[100vh] w-full sm:flex-wrap relative items-stretch">
+      <div className=" bg-darkblue min-h-[100vh] hidden lg:flex lg:w-[50%] w-[100%] flex-col justify-between pt-2">
         <div className="pt-[30px] xl:pt-[60px] px-[60px]">
           <h2 className="text-white">{LANG_AUTH_PANEL_LAYOUT_HEADING}</h2>
         </div>
-        <img src={HeroVector} alt="Login" className="w-[73%] left-0 absolute bottom-[12%]" />
+        <img src={HeroVector} alt="Login" className="w-[80%] left-0 mb-[12%]" />
       </div>
 
-      <div className="flex flex-col items-center h-[100vh] lg:w-[50%] w-[100%] bg-gradient-to-b from-gradientfrom to-gradientto">
+      <div className="flex flex-col items-center min-h-[100vh] lg:w-[50%] w-[100%] bg-gradient-to-b from-gradientfrom to-gradientto">
         <div className="pt-[30px] xl:pt-[60px] px-[60px]">
           <img src={Logo} alt="logo" />
         </div>
