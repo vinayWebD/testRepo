cc1e9613a4d15aacd13b78a240156563d835a2a4
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-09-29 18:02:11 +0530

    fixed the header UI

--- src/components/Icons/DownCaret.jsx
+++ src/components/Icons/DownCaret.jsx
@@ -1,7 +1,21 @@
 const DownCaret = () => {
   return (
-    <svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M6.5 7L0.870835 0.249999L12.1292 0.25L6.5 7Z" fill="white" />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Frame" clipPath="url(#clip0_1950_3150)">
+        <path
+          id="Vector"
+          d="M6 9L12 15L18 9"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1950_3150">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 };
