7e49c6a586adbb1063a6cd08c2f5df55a0749176
ajay palsaniya <ajay.palsaniya@dianapps.com>
2023-10-04 10:53:20 +0530

    add more interest ui

--- src/components/TextArea/index.jsx
+++ src/components/TextArea/index.jsx
@@ -14,7 +14,7 @@ const TextArea = ({
       {label && <label style={{ color: labelFontColor }}>{label}</label>}
       <textarea
         {...props}
-        className={`${className} border border-customGray px-[15px] py-[12px] text-area-input resize-none ${width} ${height} focus:border-blue-500 focus:outline-none${
+        className={`${className} border border-customGray rounded-[8px] px-[15px] py-[12px] text-area-input resize-none ${width} ${height} focus:border-blue-500 focus:outline-none${
           helperText ? 'haserror' : ''
         }`}
       ></textarea>
--- src/pages/Work/InterestsTabContent.jsx
+++ src/pages/Work/InterestsTabContent.jsx
@@ -9,7 +9,7 @@ import { UploadIcon } from '../../components/Icons/UploadIcon';
 // import { CertificateIcon } from '../../components/Icons/CertificateIcon';
 // import { ExperienceIcon } from '../../components/Icons/ExperienceIcon';
 // import { MediaIcon } from '../../components/Icons/MediaIcon';
-import InputBox from '../../components/InputBox';
+// import InputBox from '../../components/InputBox';
 import Modal from '../../components/Modal';
 import TextArea from '../../components/TextArea';
 // import { CertificateContent } from './CertificateContent';
@@ -20,7 +20,7 @@ import TextArea from '../../components/TextArea';
 export function InterestsTabContent() {
   const ref = useRef();
   const [career, setCareer] = useState('');
-  const [isSkillModalOpen, setIsSkillModalOpen] = useState(false);
+  // const [isSkillModalOpen, setIsSkillModalOpen] = useState(false);
   const [isLinksModalOpen, setIsLinksModalOpen] = useState(false);
   return (
     <div className="py-[36px] px-[70px] bg-bluebg">
@@ -41,7 +41,7 @@ export function InterestsTabContent() {
       </div>
 
       <div className="flex items-center mt-8 mb-5">
-        <div className="w-[155px] form-title">Career Title</div>
+        <div className="w-[155px] form-title">Title</div>
         <div className="grow">
           <TextArea
             width="w-full md:w-[500px]"
@@ -77,7 +77,6 @@ export function InterestsTabContent() {
       </div>
       <div className="flex items-center mt-8 mb-5">
         <div className="w-[155px] form-title">Upload Video</div>
-
         <div
           className="py-[16px] w-[240px] border-dashed border border-blueprimary rounded-lg cursor-pointer text-center"
           onClick={() => ref.current.click()}
@@ -99,8 +98,11 @@ export function InterestsTabContent() {
           />
         </div>
       </div>
+      <div className="grid justify-items-end">
+        <OutlinedButton label="Save" showArrowIcon={false} />
+      </div>
       <div>
-        <div className="mt-[36px] flex justify-between flex-wrap">
+        <div className="mt-[24px] flex justify-between flex-wrap">
           <div className="flex gap-4 flex-wrap">
             <div>
               <OutlinedButton
@@ -122,7 +124,7 @@ export function InterestsTabContent() {
       </div>
       <Modal
         isTitle={true}
-        title="Add Links"
+        title="Add Interests"
         isOpen={isLinksModalOpen}
         onClose={() => setIsLinksModalOpen(false)}
         width="max-w-[472px]"
@@ -130,41 +132,66 @@ export function InterestsTabContent() {
       >
         <>
           <div className="px-6">
-            <div className="pb-6">
-              <InputBox label="Domain" placeholder="Enter Domain" />
-            </div>
-            <div className="pb-4">
-              <InputBox label="URL" placeholder="https://" />
-            </div>
-            <div className="grid justify-items-end pb-4">
-              <OutlinedButton label="Add New" showArrowIcon={false} />
-            </div>
-          </div>
-
-          <div className="bg-greymedium h-[1px] w-full" />
-          <div className="grid justify-items-end pt-6 pb-5 px-6">
-            <Button label="Save" showArrowIcon={false} />
-          </div>
-        </>
-      </Modal>
-      <Modal
-        isTitle={true}
-        title="Add Skills"
-        isOpen={isSkillModalOpen}
-        onClose={() => setIsSkillModalOpen(false)}
-        width="max-w-[472px]"
-        padding={0}
-      >
-        <>
-          <div className="px-6">
-            <div className="pb-6">
-              <InputBox label="Skill" placeholder="Enter Skill" />
+            <label>Title</label>
+            <div className="grow">
+              <TextArea
+                width="w-full md:w-full"
+                placeholder="Enter Title"
+                value={career}
+                onChange={(e) => setCareer(e.target.value)}
+              />
             </div>
           </div>
 
+          <div className="mb-2 px-6">
+            <label>Upload Photo</label>
+            <div
+              className="py-[16px] w-full border-dashed border border-blueprimary rounded-lg cursor-pointer text-center"
+              onClick={() => ref.current.click()}
+              label="Attach Document"
+            >
+              <div className="flex items-center justify-center">
+                <span className="mr-2">
+                  <UploadIcon />
+                </span>
+                <span className="upload-btn">Upload jpg/pdf</span>
+              </div>
+              <input
+                className="hidden"
+                id="attach-document"
+                multiple
+                ref={ref}
+                type="file"
+                // onChange={handleFileEvent}
+              />
+            </div>
+          </div>
+          <div className="mb-5 px-6">
+            <label>Upload Video</label>
+            <div
+              className="py-[16px] w-full border-dashed border border-blueprimary rounded-lg cursor-pointer text-center"
+              onClick={() => ref.current.click()}
+              label="Attach Document"
+            >
+              <div className="flex items-center justify-center">
+                <span className="mr-2">
+                  <UploadIcon />
+                </span>
+                <span className="upload-btn">Upload Video</span>
+              </div>
+              <input
+                className="hidden"
+                id="attach-document"
+                multiple
+                ref={ref}
+                type="file"
+                // onChange={handleFileEvent}
+              />
+            </div>
+          </div>
           <div className="bg-greymedium h-[1px] w-full" />
-          <div className="grid justify-items-end pt-6 pb-5 px-6">
-            <Button label="Save" showArrowIcon={false} />
+          <div className="grid justify-items-end px-6 pt-6">
+            <OutlinedButton label="Save" showArrowIcon={false} />
           </div>
         </>
       </Modal>
