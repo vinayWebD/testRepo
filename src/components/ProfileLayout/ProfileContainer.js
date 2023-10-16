3976d6359c162116373984c3fbff73052719d9a0
Neha Saini <neha.saini@dianapps.com>
2023-10-16 17:54:17 +0530

    dummy image added

--- src/components/ProfileLayout/ProfileContainer.js
+++ src/components/ProfileLayout/ProfileContainer.js
@@ -4,6 +4,7 @@ import Card from '../common/Card';
 import Avatar from '../common/Avatar';
 import location from '../../assets/images/location.svg';
 import mail from '../../assets/images/mail.svg';
+import dummy from '../../assets/images/dummy.jpeg';
 import edit from '../../assets/images/editIcon.svg';
 
 const ProfileContainer = ({ userData }) => {
@@ -11,11 +12,11 @@ const ProfileContainer = ({ userData }) => {
     <Card classNames="lg:block py-4 px-2 md:px-4 relative">
       <div className="block gap-4">
         <div className='bg-iconBackground p-1 rounded w-fit absolute right-[5%]'>
-          <img src={edit} alt="edit" />
+          <img src={edit} alt="edit" className='cursor-pointer' />
         </div>
         <Avatar
           classNames="w-[40%] h-[40%] max-w-[100px] max-h-[100px] m-auto"
-          image={userData?.profile_picture_url}
+          image={userData?.profile_picture_url ? userData?.profile_picture_url : dummy}
         />
         <div className="flex flex-col gap-1 overflow-hidden text-center pt-2">
           <p className="text-greydark text-[14px] md:text-[20px] font-medium overflow-hidden truncate capitalize">
@@ -24,9 +25,11 @@ const ProfileContainer = ({ userData }) => {
           <h4 className="font-normal text-greydark text-[12px] md:text-[14px]">
             UI/UX Designer | Influencer at Masco{' '}
           </h4>
-          <h6 className="flex font-normal text-greydark text-[8px] md:text-[10px] justify-center items-center">
-            <img src={location} alt="location" className='pr-1' />{userData?.location}
-          </h6>
+          {
+            userData?.location && <h6 className="flex font-normal text-greydark text-[8px] md:text-[10px] justify-center items-center">
+              <img src={location} alt="location" className='pr-1' />{userData?.location}
+            </h6>
+          }
           <h6 className="flex font-normal text-greydark text-[8px] md:text-[10px] justify-center items-center">
             <img src={mail} alt="mail" className='pr-1' />{userData?.email}
           </h6>
