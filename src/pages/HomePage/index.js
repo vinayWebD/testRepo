7690b50b4f8645db2a074b5c2145027b1016d82b
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-10-23 12:22:27 +0530

    minor improvements

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -247,7 +247,7 @@ const HomePage = () => {
         padding="p-0"
         titleClassNames=""
         titleParentClassNames="md:m-3 m-0"
-        height="h-[calc(100vh-60px)] md:h-auto"
+        height="h-[calc(100vh-120px)] md:h-auto"
       >
         <CreatePostLayout
           closePopupHandler={() => {
