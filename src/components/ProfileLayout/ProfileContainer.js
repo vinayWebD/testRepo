8b87246d12f4f4bfc2f04a02208f8c9ab773202d
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-11-16 13:17:34 +0530

    follow unfollow other user

--- src/components/ProfileLayout/ProfileContainer.js
+++ src/components/ProfileLayout/ProfileContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import Card from '../common/Card';
 import Avatar from '../common/Avatar';
 import location from '../../assets/images/location.svg';
@@ -9,9 +9,65 @@ import Dropdown from '../common/Dropdown';
 import OutlinedButton from '../common/OutlinedButton';
 import Modal from '../../components/Modal';
 import EditProfile from '../../components/EditProfile';
+import { useDispatch } from 'react-redux';
+import {
+  followOtherUserDispatcher,
+  unfollowOtherUserDispatcher,
+} from '../../redux/dispatchers/otherUserDispatcher';
+import { getErrorMessage, successStatus } from '../../common';
+import { ToastNotifyError, ToastNotifySuccess } from '../Toast/ToastNotify';
 
-const ProfileContainer = ({ userData, isOtherUser = false }) => {
+const ProfileContainer = ({
+  userData,
+  isOtherUser = false,
+  reloadAfterFollowUnfollow = () => {},
+}) => {
   const [isEditingModalOpen, setIsEditingModalOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const followUnfollowHandler = async () => {
+    let response;
+    if (userData?.followStatus?.isApproved) {
+      response = (await dispatch(unfollowOtherUserDispatcher({ id: userData?.id }))) || {};
+    } else if (userData?.followStatus?.isApproved === undefined) {
+      response = (await dispatch(followOtherUserDispatcher({ id: userData?.id }))) || {};
+    }
+
+    const { status, data } = response;
+
+    if (successStatus(status)) {
+      if (!data?.data?.isApproved) {
+        ToastNotifySuccess('A follow request has been sent');
+      }
+      await reloadAfterFollowUnfollow();
+    } else {
+      const errormsg = getErrorMessage(data);
+      if (errormsg) {
+        ToastNotifyError(errormsg);
+      }
+    }
+  };
+
+  const memoizedAvatar = useMemo(
+    () => (
+      <Avatar
+        classNames="w-[40%] h-[40%] max-w-[100px] max-h-[100px] m-auto"
+        image={userData?.profilePictureUrl}
+        name={`${userData?.firstName} ${userData?.lastName}`}
+      />
+    ),
+    [userData?.id],
+  );
+
+  const followUnfollowLabel = () => {
+    if (userData?.followStatus?.isApproved) {
+      return 'Following';
+    } else if (userData?.followStatus?.isApproved === false) {
+      return 'Request Sent';
+    } else {
+      return 'Follow';
+    }
+  };
 
   return (
     <Card classNames="lg:block py-4 px-2 md:px-4 relative">
@@ -35,11 +91,7 @@ const ProfileContainer = ({ userData, isOtherUser = false }) => {
           </div>
         )}
 
-        <Avatar
-          classNames="w-[40%] h-[40%] max-w-[100px] max-h-[100px] m-auto"
-          image={userData?.profilePictureUrl}
-          name={`${userData?.firstName} ${userData?.lastName}`}
-        />
+        {memoizedAvatar}
         <div className="flex flex-col gap-1 overflow-hidden text-center pt-2">
           <p className="text-greydark text-[14px] md:text-[20px] font-semibold overflow-hidden truncate capitalize">
             {userData?.firstName} {userData?.lastName}
@@ -64,7 +116,8 @@ const ProfileContainer = ({ userData, isOtherUser = false }) => {
             <div className="flex gap-[7%] justify-center mt-2">
               <OutlinedButton label={'Message'} />
               <OutlinedButton
-                label={'Follow'}
+                onClick={() => followUnfollowHandler()}
+                label={followUnfollowLabel()}
                 additionalClassNames="!bg-blueprimary text-white text-[16px]"
               />
             </div>
