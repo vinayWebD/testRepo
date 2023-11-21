c439e879993bc04385be4e370f3c874bde64aa90
Neha Saini <neha.saini@dianapps.com>
2023-11-21 22:30:18 +0530

    firebase console added

---
+++ src/services/notificationService.js
@@ -0,0 +1,21 @@
+import NETWORK_CONSTANTS from '../constants/networkConstants';
+import apiUtility from '../utils/network/apiUtility';
+
+const { NOTIFICATION_LIST } = NETWORK_CONSTANTS;
+
+/**
+ * Function for calling API for searching a user
+ * @param {*} param
+ * @returns
+ */
+
+const notificationListing = async ({ page = 1, limit = 20 }) => {
+  try {
+    const data = await apiUtility(NOTIFICATION_LIST, 'GET', { page, limit });
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { notificationListing };
