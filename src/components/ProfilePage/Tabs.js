b3e2b052f7b2fab1b4668c92e5fb3fd4af355a56
Neha Saini <neha.saini@dianapps.com>
2023-11-21 13:00:24 +0530

    network ui updates

--- src/components/ProfilePage/Tabs.js
+++ src/components/ProfilePage/Tabs.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Card from '../common/Card';
-
+import postIcon from '../../assets/images/postIcon.svg'
+import workIcon from '../../assets/images/workIcon.svg'
+import interestIcon from '../../assets/images/interestIcon.svg'
+import myselfIcon from '../../assets/images/myselfIcon.svg'
 const Tabs = ({ tab = '', updateTab = () => { }, other = false }) => {
   return (
     <Card classNames="flex justify-around overflow-hidden text-center" bottomNotRound={true}>
@@ -10,7 +13,7 @@ const Tabs = ({ tab = '', updateTab = () => { }, other = false }) => {
         onClick={() => updateTab('post')}
         style={{ fontWeight: '500' }}
       >
-        Posts
+        <span className='block md:hidden flex items-center justify-center'><img src={postIcon} alt="postIcon" /></span><span className='hidden md:flex items-center justify-center'>Posts</span>
       </div>
       <div
         className={`w-[33.3%] p-4 cursor-pointer ${tab === 'work' ? 'border-b-4 border-blueprimary text-blueprimary' : ''
@@ -18,7 +21,7 @@ const Tabs = ({ tab = '', updateTab = () => { }, other = false }) => {
         onClick={() => updateTab('work')}
         style={{ fontWeight: '500' }}
       >
-        Work
+        <span className='block md:hidden flex items-center justify-center'><img src={workIcon} alt="workIcon" /></span><span className='hidden md:flex items-center justify-center'>Work</span>
       </div>
       <div
         className={`w-[33.3%] p-4 cursor-pointer ${tab === 'interest' ? 'border-b-4 border-blueprimary text-blueprimary' : ''
@@ -26,7 +29,7 @@ const Tabs = ({ tab = '', updateTab = () => { }, other = false }) => {
         onClick={() => updateTab('interest')}
         style={{ fontWeight: '500' }}
       >
-        Interests
+        <span className='block md:hidden flex items-center justify-center'><img src={interestIcon} alt="interestIcon" /></span><span className='hidden md:flex items-center justify-center'>Interests</span>
       </div>
       <div
         className={`w-[33.3%] p-4 cursor-pointer ${tab === 'myself' ? 'border-b-4 border-blueprimary text-blueprimary' : ''
@@ -34,7 +37,8 @@ const Tabs = ({ tab = '', updateTab = () => { }, other = false }) => {
         onClick={() => updateTab('myself')}
         style={{ fontWeight: '500' }}
       >
-        {other ? 'About Self' : 'Myself'}
+        <span className='block md:hidden flex items-center justify-center'><img src={myselfIcon} alt="myselfIcon" /></span><span className='hidden md:flex items-center justify-center'>{other ? 'About Self' : 'Myself'}</span>
+
       </div>
     </Card>
   );
