e82dfecb36ddd7b9d77a7ba621bdf90c7e4051f4
Neha Saini <neha.saini@dianapps.com>
2023-11-22 10:51:57 +0530

    firebase fcm issue checking

--- src/firebase.js
+++ src/firebase.js
@@ -9,24 +9,24 @@ const messaging = getMessaging(app);
 export const Sendrequest = () => {
   Notification.requestPermission().then(async (permission) => {
     console.log('permission', permission)
-    // if (permission === 'granted') {
-    try {
-      const currentToken = await getToken(messaging, {
-        vapidKey:
-          'BNEr8fsavW_uQMUS_NnllNCekCYO1_MybA1Cizb5noGiko09Rj96yrbVayebnZ2EEEf3FkF8rIU7p9fug9XSJ-0',
-      });
-      if (currentToken) {
-        localStorage.setItem('fcm', currentToken)
-        console.log('Client Token: ', currentToken);
-      } else {
-        console.log('Failed to generate the registration token.');
+    if (permission === 'granted') {
+      try {
+        const currentToken = await getToken(messaging, {
+          vapidKey:
+            'BNEr8fsavW_uQMUS_NnllNCekCYO1_MybA1Cizb5noGiko09Rj96yrbVayebnZ2EEEf3FkF8rIU7p9fug9XSJ-0',
+        });
+        if (currentToken) {
+          localStorage.setItem('fcm', currentToken)
+          console.log('Client Token: ', currentToken);
+        } else {
+          console.log('Failed to generate the registration token.');
+        }
+      } catch (err) {
+        console.log('An error occurred when requesting to receive the token', err);
       }
-    } catch (err) {
-      console.log('An error occurred when requesting to receive the token', err);
+    } else {
+      console.log('User Permission Denied.');
     }
-    // } else {
-    //   console.log('User Permission Denied.');
-    // }
   });
 };
 
