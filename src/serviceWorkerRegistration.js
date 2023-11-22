4164c55fb08e2341ddb399127c639fd37a8ff12e
Neha Saini <neha.saini@dianapps.com>
2023-11-22 11:29:54 +0530

    service worker console added

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,9 +1,10 @@
+// serviceWorkerRegistration.js
+
 export const register = () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('../public/firebase-messaging-sw.js', { scope: '/' })
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
       .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
+        console.log('Service Worker registered:', registration);
       })
       .catch((error) => {
         console.error('Error registering service worker:', error);
