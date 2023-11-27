c97937dfa1c8a0f086721a3ecb9907d219ba7356
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-11-27 11:55:05 +0530

    invite people text changes

--- src/pages/HomePage/NotificationSection.js
+++ src/pages/HomePage/NotificationSection.js
@@ -21,7 +21,7 @@ const NotificationSection = () => {
             <AddFriendIcon />
             <p className="font-semibold text-xl">{BTNLBL_INVITE_PEOPLE}</p>
           </div>
-          <p className="text-sm">Lorem ipsum dolor sit amet consectetur.</p>
+          <p className="text-sm">Please invite your friends and family to join PurDriven.</p>
         </div>
         <div
           className="p-3 text-blueprimary text-base font-semibold text-center cursor-pointer hover:opacity-70"
