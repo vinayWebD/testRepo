68ab53efa058ca2ba53f73c53207a7df4983e620
Ayushi Dangayach <ayushi.dangayach@dianapps.com>
2023-12-07 17:55:46 +0530

    fixed ui

--- src/components/ProfileLayout/FollowerContainer.js
+++ src/components/ProfileLayout/FollowerContainer.js
@@ -13,7 +13,7 @@ const FollowerContainer = ({
     <Card classNames="lg:block py-6 px-1 md:px-4 my-5">
       <div className="block gap-4">
         <div className="flex justify-around gap-4 overflow-hidden text-center">
-          <div className="block" onClick={onFollowersClick}>
+          <div className="block cursor-pointer" onClick={onFollowersClick}>
             <div className="font-normal text-greydark text-[11px] md:text-[12px] xl:text-[13px]">
               {followerCount}
             </div>
@@ -22,7 +22,7 @@ const FollowerContainer = ({
             </div>
           </div>
           <div className="block border-r border-greymedium"></div>
-          <div className="block" onClick={onFollowingClick}>
+          <div className="block cursor-pointer" onClick={onFollowingClick}>
             <div className="font-normal text-greydark text-[11px] md:text-[12px] xl:text-[13px]">
               {followingCount}
             </div>
@@ -31,7 +31,7 @@ const FollowerContainer = ({
             </div>
           </div>
           <div className="block border-greymedium border-l"></div>
-          <div className="block" onClick={onConnectionsClick}>
+          <div className="block cursor-pointer" onClick={onConnectionsClick}>
             <div className="font-normal text-greydark text-[11px] md:text-[12px] xl:text-[13px]">
               {connectionCount}
             </div>
